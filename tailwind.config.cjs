78d2925c3c74bbc269bed6c16d257649ea758e82
UtkarshGaikwad11 <utkarshgaikwad1997@gmail.com>
2025-09-19 16:14:35 +0530

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        secondary: ['"Poppins"', "sans-serif"],
+        anton: ['"Anton"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
